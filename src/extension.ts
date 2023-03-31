a66cc6037f9fe615f052321b1526a0b7fd15ef22
Vincent Kraus <mail@vinckr.com>
2023-03-31 22:12:00 +0200

    feat: ory version

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,49 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
+import { exec } from "child_process";
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  // Use the console to output diagnostic information (console.log) and errors (console.error)
+  // This line of code will only be executed once when your extension is activated
+  console.log('Congratulations, your extension "ory" is now active!');
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "ory" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('ory.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from ory!');
-	});
-
-	context.subscriptions.push(disposable);
+  // The command has been defined in the package.json file
+  // Now provide the implementation of the command with registerCommand
+  // The commandId parameter must match the command field in package.json
+  let disposableHelloWorld = vscode.commands.registerCommand(
+    "ory.helloWorld",
+    () => {
+      // The code you place here will be executed every time your command is executed
+      vscode.window.showInformationMessage("Hello World from ory!");
+    }
+  );
+  let disposableOryVersion = vscode.commands.registerCommand(
+    "ory.version",
+    () => {
+      runOryVersion();
+    }
+  );
+  context.subscriptions.push(disposableHelloWorld, disposableOryVersion);
 }
 
 // This method is called when your extension is deactivated
 export function deactivate() {}
+
+function runOryVersion() {
+  exec("ory version", (error: Error | null, stdout: string, stderr: string) => {
+    if (error) {
+      console.log(`error: ${error.message}`);
+      return;
+    }
+    if (stderr) {
+      console.log(`stderr: ${stderr}`);
+      return;
+    }
+    vscode.window.showInformationMessage(`${stdout}`, {
+      modal: true,
+    });
+  });
+}
